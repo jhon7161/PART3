a30c20d950b6065c456e9cd785cf046a2c8c3e23
appaint <jhoneba@gmail.com>
2024-03-17 15:43:12 -0500

    part3.12Base de datos de línea de comandos-corregida

--- demo.js
+++ demo.js
@@ -2,8 +2,9 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const password = process.env.DB_PASSWORD;
-const name = process.argv[2];
-const number = process.argv[3];
+const name = process.argv[3];
+const number = process.argv[4];
+const userPassword = process.argv[2];
 
 const url = `mongodb+srv://desarrollador19jhon:${password}@appphonebook.qaaig53.mongodb.net/sample_airbnb`;
 
@@ -14,11 +15,28 @@ mongoose.connect(url)
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
+  password: String,
 });
 
 const Person = mongoose.model('Person', personSchema);
 
-if (process.argv.length < 4) {
+// Validaciones
+if (!name || typeof name !== 'string') {
+  console.error('El nombre proporcionado no es válido');
+  process.exit(1);
+}
+
+if (!number || typeof number !== 'string') {
+  console.error('El número proporcionado no es válido');
+  process.exit(1);
+}
+
+if (!userPassword || typeof userPassword !== 'string') {
+  console.error('La contraseña proporcionada no es válida');
+  process.exit(1);
+}
+
+if (process.argv.length < 5) {
   Person.find({}).then((result) => {
     console.log('phonebook:');
     result.forEach((person) => {
@@ -30,6 +48,7 @@ if (process.argv.length < 4) {
   const person = new Person({
     name: name,
     number: number,
+    password: userPassword,
   });
 
   person.save().then(() => {
